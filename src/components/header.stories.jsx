7b3ed7b7bb7642c1500c0b3b476b555c825a3f80
kunalja <kunaljain@utexas.edu>
2019-12-06 16:28:54 -0600

    Remove stock header

--- src/components/header.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-
-import Header from "./header"
-
-storiesOf("Header", module)
-  .add("default", () => <Header />)
-  .add("error", () => <Header siteTitle="Title Prop" />)
