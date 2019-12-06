f65421c83c48a1edd67dbe016bcb47553f79c70f
kunalja <kunaljain@utexas.edu>
2019-12-06 16:52:25 -0600

    navbar long title story

--- src/components/navbar/navbar.stories.jsx
+++ src/components/navbar/navbar.stories.jsx
@@ -6,3 +6,4 @@ import Navbar from "./navbar"
 storiesOf("Navbar", module)
   .add("Default", () => <Navbar />)
   .add("Title prop", () => <Navbar siteTitle="Title Prop" />)
+  .add("Long Name", () => <Navbar siteTitle="I am not certain how to handle this situation: submit a pull request to https://github.com/kunalJa/gatsby-starter-math-blog/issues" />)
