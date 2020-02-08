19cb824ff84c9196fcae90d910bd099038181615
kunalja <kunaljain@utexas.edu>
2020-02-07 19:49:35 -0600

    Componentry to add a social button to a page

---
+++ src/components/social/socialButton.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import SVG from "../svg"
+import { icons } from "./icons"
+
+const SocialButton = ({ link, width, fill, socialEnum }) => {
+  return (
+    <a href={link}>
+      <SVG width={width} fill={fill} path={icons[socialEnum]} />
+    </a>
+  )
+}
+
+SocialButton.propTypes = {
+  link: PropTypes.string,
+  socialEnum: PropTypes.number.isRequired,
+  fill: PropTypes.string,
+  width: PropTypes.string,
+}
+
+SocialButton.defaultProps = {
+  link: "/",
+  fill: '#000',
+  width: '3rem',
+}
+
+export default SocialButton
