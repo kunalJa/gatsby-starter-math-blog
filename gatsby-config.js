c4422637b7ec3cc298031592df9817638179cfbf
kunalja <kunaljain@utexas.edu>
2020-03-20 08:40:47 -0500

    Place blogposts in the /blog folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog/`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         gatsbyRemarkPlugins: [`gatsby-remark-katex`],
