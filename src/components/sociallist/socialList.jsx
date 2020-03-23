dc3b5dc4176f9ca81ddbe0c79dbcfb1f907f4bb5
kunalja <kunaljain@utexas.edu>
2020-03-23 16:42:51 -0500

    Social List component

---
+++ src/components/sociallist/socialList.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import PropTypes from "prop-types"
+import SocialButton from "../socialbutton/socialButton"
+
+const SocialList = ({ width, fill, links, socialEnums }) => {
+  return (
+    <section className="mw5 center flex justify-around mv3">
+      {socialEnums.map((socialEnum, id) => {
+        return (
+          <div>
+            <SocialButton key={socialEnum} width={width} fill={fill} link={links[id]} socialEnum={socialEnum} />
+          </div>
+        )
+      })}
+    </section>
+  )
+}
+
+SocialList.propTypes = {
+  links: PropTypes.arrayOf(PropTypes.string).isRequired,
+  socialEnums: PropTypes.arrayOf(PropTypes.Number).isRequired,
+  fill: PropTypes.string,
+  width: PropTypes.string,
+}
+
+SocialList.defaultProps = {
+  fill: `#000`,
+  width: `3em`,
+}
+
+export default SocialList
