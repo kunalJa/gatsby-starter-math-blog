c102ade29e825a712c176c8aa2ec05ac35e71a74
kunalja <kunaljain@utexas.edu>
2020-03-23 19:51:39 -0500

    Hero component

---
+++ src/components/herobox/heroBox.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import SocialList from "../sociallist/socialList"
+
+const HeroBox = ({ siteTitle, author, links, socialEnums }) => {
+  return (
+    <div
+      className="center mw5 mw7-l pa2"
+      style={
+        {
+          overflowWrap: "break-word",
+          wordWrap: "break-word",
+        }
+      }
+    >
+      <h1 className="f1 f-subheadline-l fw8 tc-l tracked ma0">
+        {siteTitle.split("").map(c => {
+          const ascii = c.charCodeAt(0)
+          const alphabetical = (ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123)
+          return alphabetical ? <span>{c}</span> : <span className="light-red">{c}</span>
+        })}
+      </h1>
+      <h3 className="f4 fw2 tr mr6-l">By: {author}</h3>
+      {[...Array(Math.ceil(Math.min(links.length, socialEnums.length) / 3))]
+        .map((_, i) => {
+          return (
+            <SocialList links={links.slice(i * 3, (i + 1) * 3)} socialEnums={socialEnums.slice(i * 3, (i + 1) * 3)} />
+          )
+        })}
+    </div>
+  )
+}
+
+HeroBox.propTypes = {
+  siteTitle: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  links: PropTypes.arrayOf(PropTypes.string),
+  socialEnums: PropTypes.arrayOf(PropTypes.Number),
+}
+
+HeroBox.defaultProps = {
+  links: [],
+  socialEnums: []
+}
+
+export default HeroBox
---
+++ src/components/herobox/heroBox.stories.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+
+import HeroBox from "./heroBox"
+
+storiesOf("HeroBox", module)
+  .add("Alphabetical Title", () => <HeroBox siteTitle="Math Blog" author="@kunalja" />)
+  .add("Title with Symbols", () => <HeroBox siteTitle="M@th B|og" author="@kunalja" />)
+  .add("Long Title", () => <HeroBox siteTitle="supercalifragilisticexpialidocious is my favourite word!" author="@kunalja" />)
+  .add("With One Social Link", () => <HeroBox siteTitle="M@th B|og" author="@kunalja" links={["https://www.facebook.com"]} socialEnums={[0]} />)
+  .add("With Three Social Links", () => <HeroBox siteTitle="M@th B|og" author="@kunalja" links={["https://www.facebook.com", "https://www.gatsbyjs.com", "https://www.github.com"]} socialEnums={[0, 1, 2]} />)
+  .add("With Eight Social Links", () => <HeroBox siteTitle="M@th B|og" author="@kunalja" links={["https://www.facebook.com", "https://www.gatsbyjs.com", "https://www.github.com", "https://www.imdb.com", "https://www.linkedin.com", "https://www.reddit.com", "https://www.skype.com", "https://www.slack.com"]} socialEnums={[0, 1, 2, 3, 4, 5, 6, 7]} />)
