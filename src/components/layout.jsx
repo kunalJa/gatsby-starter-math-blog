825356046c7590d2fe8d879c46f1018c0f32a7df
kunalja <kunaljain@utexas.edu>
2020-03-23 22:34:43 -0500

    update layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Navbar from "./navbar/navbar"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
