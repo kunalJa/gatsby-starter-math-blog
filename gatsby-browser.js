5a5504f1c1bc6466aee1a62608f7f3ea4c9e0f7f
kunalja <kunaljain@utexas.edu>
2020-03-24 20:47:51 -0500

    Customise MDX theming with prism-react-renderer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,45 @@
 import "tachyons"
 import "katex/dist/katex.min.css"
+
+import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import theme from "prism-react-renderer/themes/github"
+
+/* eslint-disable */
+const component = {
+  pre: props => {
+    const className = props.children.props.className || ""
+    const matches = className.match(/language-(?<lang>.*)/)
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children}
+        language={
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ""
+        }
+        theme={theme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className + " overflow-auto"} style={style}>
+            {tokens.map((line, i) => (
+              <div className="ma0 pa0" {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => {
+                  if (key === line.length - 1) {
+                    return null
+                  }
+                  return <span {...getTokenProps({ token, key })} />
+                })}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    )
+  },
+}
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={component}>{element}</MDXProvider>
+}
