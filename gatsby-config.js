7155684586652eccf49a215d3d37fd4b91c8ba5a
kunalja <kunaljain@utexas.edu>
2020-03-25 21:50:16 -0500

    extracting config to seperate file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,10 @@
 const path = require(`path`)
 const remarkMath = require(`remark-math`)
 
-module.exports = {
-  siteMetadata: {
-    /* General: */
-    title: `Math Blog`,
-    description: `The premier math blog starter for Gatsby.`,
-    author: `@kunalja`,
-    personalDescription: `I am a student at the University of Texas. I am an aspriring software engineer hoping to enter the space of distributed system design and systems programming.`,
+const config = require(`./site.config`)
 
-    socialLinks: {
-      facebook: 0,
-      gatsby: 0,
-      github: `https://github.com/kunalJa/gatsby-starter-math-blog`,
-      imdb: 0,
-      linkedin: `https://www.linkedin.com`,
-      reddit: 0,
-      skype: 0,
-      slack: 0,
-      spotify: `https://www.spotify.com`,
-      telegram: 0,
-      twitter: `https://www.twitter.com`,
-      whatsapp: 0,
-  },
-  },
+module.exports = {
+  siteMetadata: config,
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
@@ -78,7 +59,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: false, // Enable while using `gatsby develop`
         // whitelist: ['whitelist'], // Don't remove this selector
-        ignore: ["katex/dist/katex.min.css"], // Ignore files/folders
+        // ignore: ["katex/dist/katex.min.css", "tachyons/css/tachyons.css"],
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'],
       },
     },
---
+++ site.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: `M@th B|og`,
+  description: `The premier math blog starter for Gatsby.`,
+  author: `@kunalja`,
+  personalDescription: `I am a student at the University of Texas. I am an aspriring software engineer hoping to enter the space of distributed system design and systems programming.`,
+
+  socialLinks: {
+    facebook: 0,
+    gatsby: 0,
+    github: `https://github.com/kunalJa/gatsby-starter-math-blog`,
+    imdb: 0,
+    linkedin: `https://www.linkedin.com/in/kunalja`,
+    reddit: 0,
+    skype: 0,
+    slack: 0,
+    spotify: 0,
+    telegram: 0,
+    twitter: 0,
+    whatsapp: 0,
+  }
+}
